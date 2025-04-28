45f09fbe153b624afc7e08027f8281cc679fdd43
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-04-28 19:08:41 +0200

    added alot of style changes for the client

--- client/vite-project/src/App.tsx
+++ client/vite-project/src/App.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { catDB, catFormData } from "./types/catDbType";
 import { FormStateType } from "./types/formActiveType";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaRegTrashCan } from "react-icons/fa6";
 
 import "./App.css";
 
@@ -106,87 +108,165 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setIsFormActive({ state: true, event: "add" })}>
-        Add a cat
-      </button>
-      {catDb.map((cat) => {
-        return (
-          <div className="flex" key={cat._id}>
-            <h2>{cat.name}</h2>
-            <button onClick={() => handleDelete(cat._id)}>Delete</button>
-            <button onClick={() => updateFormData(cat)}>Update</button>
-          </div>
-        );
-      })}
+      {!isFormActive.state && (
+        <div className="mb-8">
+          <button
+            className="text-xl bg-green-500 px-6 py-4 rounded-xl font-bold cursor-pointer transition-transform duration-300 hover:-translate-y-1"
+            onClick={() => setIsFormActive({ state: true, event: "add" })}
+          >
+            Add a cat
+          </button>
+        </div>
+      )}
+
+      {!isFormActive.state && (
+        <div className="flex gap-20 flex-wrap justify-center ">
+          {catDb.map((cat) => {
+            return (
+              <div
+                className="flex items-center w-[310px] h-[410px] flex-col rounded-2xl bg-white "
+                key={cat._id}
+              >
+                <div className="w-full ">
+                  <img
+                    className="w-full rounded-t-lg h-48 object-cover"
+                    src={cat.imageUrl}
+                  ></img>
+                </div>
+                <div className="w-full">
+                  <h2 className="text-black text-3xl font-medium text-center mt-4 mb-5">
+                    {cat.name}
+                  </h2>
+                  <div className="text-black grid grid-cols-2 gap-2 mb-4 text-lg">
+                    <div className="flex gap-2 justify-center">
+                      <h3 className="font-bold">Age:</h3>
+                      <span className="text-[#64748b] font-medium">
+                        {cat.age}
+                      </span>
+                    </div>
+                    <div className="flex gap-2">
+                      <h3 className="font-bold">Gender:</h3>
+                      <span className="text-[#64748b] font-medium">
+                        {cat.gender}
+                      </span>
+                    </div>
+                    <div className="flex gap-2 col-span-2 justify-self-center">
+                      <h3 className="font-bold">Breed:</h3>
+                      <span className="text-[#64748b]">{cat.breed}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-center gap-2 w-full px-4">
+                    <button
+                      className="bg-[#dc2626] font-bold cursor-pointer flex items-center gap-1.5 px-6 py-2 w-full rounded-lg transition-transform duration-300 hover:-translate-y-1"
+                      onClick={() => handleDelete(cat._id)}
+                    >
+                      <FaRegTrashCan />
+                      Remove
+                    </button>
+                    <button
+                      className="flex items-center gap-1.5 font-bold cursor-pointer bg-[#6d28d9] px-6 py-2 w-full rounded-lg transition-transform duration-300 hover:-translate-y-1"
+                      onClick={() => updateFormData(cat)}
+                    >
+                      <FaPencilAlt /> Update
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
 
       {isFormActive.state && (
         <form
           onSubmit={(e) => handleFormSubmit(e)}
-          className="flex flex-col gap-3"
+          className="flex flex-col gap-3 text-xl"
         >
-          <label className="text-start" htmlFor="cat-name">
-            Name
-          </label>
-          <input
-            className="border-2 border-black pl-1.5 placeholder:text-white"
-            type="text"
-            id="cat-name"
-            placeholder="Name"
-            onChange={(e) => handleChange(e)}
-            value={formData.name}
-            name="name"
-            required
-          />
-          <label htmlFor="cat-age"></label>
-          <input
-            className="border-2 border-black pl-1.5 placeholder:text-white"
-            type="number"
-            id="cat-age"
-            placeholder="Age"
-            onChange={(e) => handleChange(e)}
-            value={formData.age}
-            name="age"
-            required
-          />
-          <label htmlFor="gender-select"></label>
-          <select
-            className="border-2 border-black pl-1.5 placeholder:text-white"
-            name="gender"
-            id="gender-select"
-            onChange={(e) => handleChange(e)}
-            value={formData.gender}
-            required
-          >
-            <option className="bg-black" value="Male">
-              Male
-            </option>
-            <option className="bg-black" value="Female">
-              Female
-            </option>
-          </select>
-          <label htmlFor="cat-breed"></label>
-          <input
-            className="border-2 border-black pl-1.5 placeholder:text-white"
-            type="text"
-            id="cat-breed"
-            placeholder="Breed"
-            onChange={(e) => handleChange(e)}
-            value={formData.breed}
-            name="breed"
-            required
-          />
-          <label htmlFor="image-url"></label>
-          <input
-            className="border-2 border-black pl-1.5 placeholder:text-white"
-            type="text"
-            id="image-url"
-            placeholder="Image Url"
-            onChange={(e) => handleChange(e)}
-            value={formData.imageUrl}
-            name="imageUrl"
-          />
-          <button type="submit">Submit</button>
+          <div className="flex flex-col ">
+            <label className="text-start" htmlFor="cat-name">
+              Name
+            </label>
+            <input
+              className="border-2 border-black pl-1.5 placeholder:text-[#64748b] p-2"
+              type="text"
+              id="cat-name"
+              placeholder="E.g. Mittens"
+              onChange={(e) => handleChange(e)}
+              value={formData.name}
+              name="name"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-start" htmlFor="cat-age">
+              Age
+            </label>
+            <input
+              className="border-2 border-black pl-1.5 placeholder:text-[#64748b] p-2"
+              type="number"
+              id="cat-age"
+              placeholder="2"
+              onChange={(e) => handleChange(e)}
+              value={formData.age}
+              name="age"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-start" htmlFor="gender-select">
+              Gender
+            </label>
+            <select
+              className="border-2 border-black pl-1.5 placeholder:text-[#64748b] p-2"
+              name="gender"
+              id="gender-select"
+              onChange={(e) => handleChange(e)}
+              value={formData.gender}
+              required
+            >
+              <option className="bg-black" value="Male">
+                Male
+              </option>
+              <option className="bg-black" value="Female">
+                Female
+              </option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="text-start" htmlFor="cat-breed">
+              Breed
+            </label>
+            <input
+              className="border-2 border-black pl-1.5 placeholder:text-[#64748b] p-2"
+              type="text"
+              id="cat-breed"
+              placeholder="E.g. Maine Coon"
+              onChange={(e) => handleChange(e)}
+              value={formData.breed}
+              name="breed"
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-start" htmlFor="image-url">
+              Url
+            </label>
+            <input
+              className="border-2 border-black pl-1.5 placeholder:text-[#64748b] p-2"
+              type="text"
+              id="image-url"
+              placeholder="https://placekitten.com/400/300"
+              onChange={(e) => handleChange(e)}
+              value={formData.imageUrl}
+              name="imageUrl"
+            />
+          </div>
+
+          <button className="bg-green-600 text-xl font-bold p-2" type="submit">
+            Submit
+          </button>
           <button
+            className="bg-blue-600 text-xl  font-bold p-2"
             onClick={() => {
               setIsFormActive({ state: false, event: "none" });
             }}
